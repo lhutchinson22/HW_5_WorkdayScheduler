296b56f3063b264795712653342a99a25c312770
lhutchinson22 <lbhutchinson022@gmail.com>
2020-10-28 21:19:14 -0700

    working on parsing the integer to compare to moment

--- develop/app.js
+++ develop/app.js
@@ -1,11 +1,38 @@
-console.log("sanity check");
+$(document).ready(function () {
+  console.log("sanity check");
 
-var timeblockContainer = document.getElementById("time-block");
-var fetchButton = document.getElementById("saveBtn");
-var currentDay = document.getElementById("currentDay");
+  var timeblockContainer = document.getElementById("time-block");
+  var fetchButton = document.getElementById("saveBtn");
+  var currentDay = document.getElementById("currentDay");
+  var myInputText = document.getElementById("myText");
+  var hourInteger = document.getElementById("hourInt");
 
-var now = moment().format("LLLL");
-console.log(now);
-currentDay.append(now);
+  // moment.js
+  var now = moment().format("LLLL");
+  console.log(now);
+  currentDay.append(now);
 
+  //!!!!! how to parse span id to check the num\ber
+  var parseHourInt = parseInt(hourInteger.grabText());
+  console.log(parseHourInt);
+
+  // if the time is equal to the calendar time, an attribute will be added to div
+
+  if (now === parseHourInt) {
+    console.log("This is the time");
+  }
+
+  //when savebtn is pressed save data to local storage
+  $(".saveBtn").on("click", function () {
+    console.log("save button is clicked");
+    var grabText = myInputText.textContent($("#myText"));
+
+    console.log(grabText);
+
+    // for (let i = 0; i < array.length; i++) {
+    //     const element = array[i];
+
+    // }
+  });
+});
 //dynamic html = colors and if items are saved, items are saved in hour time slot
