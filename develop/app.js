d65e4bd1e8efe8a45f8fe8a6804ca0eecde5f581
lhutchinson22 <lbhutchinson022@gmail.com>
2020-10-28 19:38:46 -0700

    appended moment

--- develop/app.js
+++ develop/app.js
@@ -1,4 +1,11 @@
 console.log("sanity check");
 
-// var moment = require("moment");
-// moment().format();
+var timeblockContainer = document.getElementById("time-block");
+var fetchButton = document.getElementById("saveBtn");
+var currentDay = document.getElementById("currentDay");
+
+var now = moment().format("LLLL");
+console.log(now);
+currentDay.append(now);
+
+//dynamic html = colors and if items are saved, items are saved in hour time slot
